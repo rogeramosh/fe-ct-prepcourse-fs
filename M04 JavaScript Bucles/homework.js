5b1530a61166529964644980b006951bc7444be4
rogeramosh <rogeramosh@outlook.es>
2023-07-26 02:59:05 -0500

    2:59am

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -102,7 +102,7 @@ function operadoresLogicos(num1, num2, num3) {
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
    if(num1>num2 && num1>num3 && num1>0) return "Numero 1 es mayor y positivo";  
-   if(num1 ===0 && num2===0 && num3===0) return "error";
+   if(num1 === 0 && num2 === 0 && num3 === 0) return "error";
    if(num1<0 ||num2<0 ||num3<0) return "Hay negativos";
   
    if(num3>num1 && num3>num2) return num3 =num3 +1;
